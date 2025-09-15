c24aa11b4e4a4ccf8da4d68b208500c67d964977
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-15 10:40:47 -0300

    fix: updated application _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,51 @@
-import {Stack} from 'expo-router';
+import {Drawer} from 'expo-router/drawer';
 
-export default function StackLayout() {
-    return (<Stack>
-        <Stack.Screen name="pomodoro" options={{headerShown: false}}/>
-    </Stack>);
+export default function Layout() {
+    return <Drawer
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: '#021123',
+            },
+            headerTintColor: '#FFFFFF',
+            drawerStyle: {
+                backgroundColor: '#021123',
+            },
+            drawerLabelStyle: {
+                color: '#FFFFFF',
+            }
+        }}>
+        <Drawer.Screen
+            name={'index'}
+            options={{
+                headerShown: false,
+                drawerItemStyle: {display: 'none'},
+            }}
+        />
+
+        <Drawer.Screen
+            name={'pomodoro'}
+            options={{
+                headerShown: true,
+                drawerLabel: 'Timer',
+                title: 'Pomodoro Timer',
+            }}
+        />
+
+        <Drawer.Screen
+            name={'tasks'}
+            options={{
+                headerShown: true,
+                drawerLabel: 'Lista de Tarefas',
+                title: 'Minhas Tarefas',
+            }}
+        />
+
+        <Drawer.Screen
+            name={'add-task'}
+            options={{
+                drawerItemStyle: {display: 'none'},
+                title: '',
+            }}
+        />
+    </Drawer>;
 }
