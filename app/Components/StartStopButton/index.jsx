35bbb248a8c2497e03977560af154d3433294c8f
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-15 08:43:45 -0300

    fix: updated the start stop button to use onPress and new icon]

--- app/Components/StartStopButton/index.jsx
+++ app/Components/StartStopButton/index.jsx
@@ -1,8 +1,9 @@
 import {StyleSheet, Pressable, Text} from "react-native";
 
-export const StartStopButton = () => {
-    return (<Pressable style={styles.button}>
-        <Text style={styles.button_text}>Comecar</Text>
+export const StartStopButton = ({onPress, title, icon}) => {
+    return (<Pressable style={styles.button} onPress={onPress}>
+        {icon}
+        <Text style={styles.button_text}>{title}</Text>
     </Pressable>)
 }
 
@@ -10,12 +11,16 @@ const styles = StyleSheet.create({
     button: {
         backgroundColor: "#B872FF",
         borderRadius: 32,
+        padding: 8,
+        flexDirection: "row",
+        gap: 12,
+        alignItems: "center",
+        justifyContent: "center",
     },
 
     button_text: {
-        color: "#000",
         fontSize: 18,
+        color: "#021123",
         textAlign: "center",
-        padding: 8,
     },
 })
