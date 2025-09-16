cafdd077143718cf543772630a12e2ad2129883b
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-16 10:53:18 -0300

    feat: used useEffects to stora and load tasks by local storage

--- components/context/TaskProvider.jsx
+++ components/context/TaskProvider.jsx
@@ -1,10 +1,43 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const TaskContext = createContext()
 
 export function TasksProvider({children}) {
 
     const [tasks, setTasks] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    const TASKS_STORAGE_KEY = 'fokus-tasks'
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem(TASKS_STORAGE_KEY);
+                const loadedData = jsonValue != null ? JSON.parse(jsonValue) : [];
+                setTasks(loadedData)
+                setIsLoaded(true)
+            } catch (e) {
+                // error reading value
+            }
+        };
+        getData()
+    }, []);
+
+    useEffect(() => {
+        const storeData = async (value) => {
+            try {
+                const jsonValue = JSON.stringify(value);
+                await AsyncStorage.setItem(TASKS_STORAGE_KEY, jsonValue);
+            } catch (e) {
+                // saving error
+            }
+        };
+
+        if (isLoaded) {
+            storeData(tasks)
+        }
+    }, [tasks]);
 
     const addTask = (description) => {
         setTasks(oldState => {
@@ -23,7 +56,7 @@ export function TasksProvider({children}) {
         setTasks(oldState => {
             return oldState.map(task => {
                 if (task.id === id) {
-                    return { ...task, completed: !task.completed }
+                    return {...task, completed: !task.completed}
                 }
                 return task
             })
@@ -37,7 +70,7 @@ export function TasksProvider({children}) {
     }
 
     return (
-        <TaskContext.Provider value={{ tasks, addTask, toggleTaskCompleted, deleteTask }}>
+        <TaskContext.Provider value={{tasks, addTask, toggleTaskCompleted, deleteTask}}>
             {children}
         </TaskContext.Provider>
     )
