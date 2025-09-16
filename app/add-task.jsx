5a8d7767b585021db40728cf3f88052aa7866eeb
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-16 09:10:11 -0300

    feat: created the add-task form, styles and keyboard configuration

--- app/add-task.jsx
+++ app/add-task.jsx
@@ -1,9 +1,85 @@
-import {Text, View} from "react-native";
+import {
+    StyleSheet,
+    Pressable,
+    Text,
+    TextInput,
+    View,
+    TouchableWithoutFeedback,
+    Platform,
+    KeyboardAvoidingView, Keyboard
+} from "react-native";
+import {IconSave} from "../components/Icons";
 
 export default function AddTaks() {
     return (
-        <View>
-            <Text>Página para adicionar uma task</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => {
+            // @ts-ignore
+            Keyboard.dismiss();
+        }}>
+            <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                <Text style={styles.text}>
+                    Adicionar uma tarefa:
+                </Text>
+                <View style={styles.container_inner}>
+                    <Text style={styles.label}>
+                        Em que você está pensando?
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        numberOfLines={10}
+                        multiline={true}
+                    />
+                    <Pressable style={styles.save_button}>
+                        <IconSave/>
+                        <Text>
+                            Salvar
+                        </Text>
+                    </Pressable>
+                </View>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#021123',
+        gap: 24,
+        alignItems: "center",
+    },
+
+    text: {
+        color: '#FFF',
+        fontSize: 24,
+        textAlign: "center",
+    },
+
+    container_inner: {
+        backgroundColor: '#98A0A8',
+        width: "90%",
+        padding: 16,
+        borderRadius: 8,
+        gap: 32,
+    },
+
+    label: {
+        color: '#021123',
+        fontSize: 18,
+        fontWeight: "600",
+    },
+
+    input: {
+        backgroundColor: "#fff",
+        padding: 16,
+        borderRadius: 8,
+        height: 150,
+    },
+
+    save_button: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        gap: 8,
+    },
+})
