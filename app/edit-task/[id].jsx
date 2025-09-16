b731acc0732d49648d8f0b20d716045335aba707
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-16 11:11:17 -0300

    feat: created the edit task form

---
+++ app/edit-task/[id].jsx
@@ -0,0 +1,118 @@
+import {
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    Pressable, StyleSheet,
+    Text,
+    TextInput,
+    TouchableWithoutFeedback,
+    View
+} from "react-native";
+import {router, useLocalSearchParams} from "expo-router";
+import {IconSave} from "../../components/Icons";
+import {useEffect, useState} from "react";
+import useTaskContext from "../../components/context/useTaskContext";
+
+export default function Id() {
+
+    const {id} = useLocalSearchParams()
+
+    const [description, setDescription] = useState();
+
+    const {tasks, updateTask} = useTaskContext();
+
+    useEffect(() => {
+        if (id && tasks && tasks.length > 0) {
+            const task = tasks.find(t => String(t.id) === String(id));
+            if (task) {
+                setDescription(task.description);
+            }
+        }
+    }, [id, tasks])
+
+    const submitTask = () => {
+        if (!description) {
+            return
+        }
+        if (id) {
+            updateTask(Number(id), description);
+        }
+        // @ts-ignore
+        Keyboard.dismiss();
+        router.navigate('/tasks')
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => {
+            // @ts-ignore
+            Keyboard.dismiss();
+        }}>
+            <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                <Text style={styles.text}>
+                    Editar tarefa:
+                </Text>
+                <View style={styles.container_inner}>
+                    <Text style={styles.label}>
+                        Em que você está pensando?
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        numberOfLines={10}
+                        multiline={true}
+                        value={description}
+                        onChangeText={setDescription}
+                    />
+                    <Pressable style={styles.save_button} onPress={submitTask}>
+                        <IconSave/>
+                        <Text>
+                            Salvar
+                        </Text>
+                    </Pressable>
+                </View>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#021123',
+        gap: 24,
+        alignItems: "center",
+    },
+
+    text: {
+        color: '#FFF',
+        fontSize: 24,
+        textAlign: "center",
+    },
+
+    container_inner: {
+        backgroundColor: '#98A0A8',
+        width: "90%",
+        padding: 16,
+        borderRadius: 8,
+        gap: 32,
+    },
+
+    label: {
+        color: '#021123',
+        fontSize: 18,
+        fontWeight: "600",
+    },
+
+    input: {
+        backgroundColor: "#fff",
+        padding: 16,
+        borderRadius: 8,
+        height: 150,
+    },
+
+    save_button: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        gap: 8,
+    },
+})
